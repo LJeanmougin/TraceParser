f93f2e3189ce22f45a674e1125bc59b644577e4a
LJeanmougin <louison.jeanmougin@irit.fr>
2025-05-27 17:36:52 +0200

    Added the missing configs in sgemm_stats.sh

--- sgemm_stats.sh
+++ sgemm_stats.sh
@@ -1,45 +1,53 @@
+python3 trace_extraction.py ../sgemm/sgemm_4w_5l/sgemm_4w ./traces/sgemm_4w_5l
 python3 trace_extraction.py ../sgemm/sgemm_4w_10l/sgemm_4w ./traces/sgemm_4w_10l
 python3 trace_extraction.py ../sgemm/sgemm_4w_25l/sgemm_4w ./traces/sgemm_4w_25l
 python3 trace_extraction.py ../sgemm/sgemm_4w_50l/sgemm_4w ./traces/sgemm_4w_50l
 python3 trace_extraction.py ../sgemm/sgemm_4w_100l/sgemm_4w ./traces/sgemm_4w_100l
 python3 trace_extraction.py ../sgemm/sgemm_4w_200l/sgemm_4w ./traces/sgemm_4w_200l
 python3 trace_extraction.py ../sgemm/sgemm_4w_400l/sgemm_4w ./traces/sgemm_4w_400l
+python3 trace_extraction.py ../sgemm/sgemm_8w_5l/sgemm_8w ./traces/sgemm_8w_5l
 python3 trace_extraction.py ../sgemm/sgemm_8w_10l/sgemm_8w ./traces/sgemm_8w_10l
 python3 trace_extraction.py ../sgemm/sgemm_8w_25l/sgemm_8w ./traces/sgemm_8w_25l
 python3 trace_extraction.py ../sgemm/sgemm_8w_50l/sgemm_8w ./traces/sgemm_8w_50l
 python3 trace_extraction.py ../sgemm/sgemm_8w_100l/sgemm_8w ./traces/sgemm_8w_100l
 python3 trace_extraction.py ../sgemm/sgemm_8w_200l/sgemm_8w ./traces/sgemm_8w_200l
 python3 trace_extraction.py ../sgemm/sgemm_8w_400l/sgemm_8w ./traces/sgemm_8w_400l
+python3 trace_extraction.py ../sgemm/sgemm_16w_5l/sgemm_16w ./traces/sgemm_16w_5l
 python3 trace_extraction.py ../sgemm/sgemm_16w_10l/sgemm_16w ./traces/sgemm_16w_10l
 python3 trace_extraction.py ../sgemm/sgemm_16w_25l/sgemm_16w ./traces/sgemm_16w_25l
 python3 trace_extraction.py ../sgemm/sgemm_16w_50l/sgemm_16w ./traces/sgemm_16w_50l
 python3 trace_extraction.py ../sgemm/sgemm_16w_100l/sgemm_16w ./traces/sgemm_16w_100l
 python3 trace_extraction.py ../sgemm/sgemm_16w_200l/sgemm_16w ./traces/sgemm_16w_200l
 python3 trace_extraction.py ../sgemm/sgemm_16w_400l/sgemm_16w ./traces/sgemm_16w_400l
+python3 trace_extraction.py ../sgemm/sgemm_32w_5l/sgemm_32w ./traces/sgemm_32w_5l
 python3 trace_extraction.py ../sgemm/sgemm_32w_10l/sgemm_32w ./traces/sgemm_32w_10l
 python3 trace_extraction.py ../sgemm/sgemm_32w_25l/sgemm_32w ./traces/sgemm_32w_25l
 python3 trace_extraction.py ../sgemm/sgemm_32w_50l/sgemm_32w ./traces/sgemm_32w_50l
 python3 trace_extraction.py ../sgemm/sgemm_32w_100l/sgemm_32w ./traces/sgemm_32w_100l
 python3 trace_extraction.py ../sgemm/sgemm_32w_200l/sgemm_32w ./traces/sgemm_32w_200l
 python3 trace_extraction.py ../sgemm/sgemm_32w_400l/sgemm_32w ./traces/sgemm_32w_400l
+python3 trace_extraction.py ../sgemm/sgemm_4w_5l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_5l
 python3 trace_extraction.py ../sgemm/sgemm_4w_10l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_10l
 python3 trace_extraction.py ../sgemm/sgemm_4w_25l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_25l
 python3 trace_extraction.py ../sgemm/sgemm_4w_50l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_50l
 python3 trace_extraction.py ../sgemm/sgemm_4w_100l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_100l
 python3 trace_extraction.py ../sgemm/sgemm_4w_200l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_200l
 python3 trace_extraction.py ../sgemm/sgemm_4w_400l/sgemm_double_buff_4w ./traces/sgemm_double_buff_4w_400l
+python3 trace_extraction.py ../sgemm/sgemm_8w_5l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_5l
 python3 trace_extraction.py ../sgemm/sgemm_8w_10l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_10l
 python3 trace_extraction.py ../sgemm/sgemm_8w_25l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_25l
 python3 trace_extraction.py ../sgemm/sgemm_8w_50l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_50l
 python3 trace_extraction.py ../sgemm/sgemm_8w_100l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_100l
 python3 trace_extraction.py ../sgemm/sgemm_8w_200l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_200l
 python3 trace_extraction.py ../sgemm/sgemm_8w_400l/sgemm_double_buff_8w ./traces/sgemm_double_buff_8w_400l
+python3 trace_extraction.py ../sgemm/sgemm_16w_5l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_5l
 python3 trace_extraction.py ../sgemm/sgemm_16w_10l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_10l
 python3 trace_extraction.py ../sgemm/sgemm_16w_25l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_25l
 python3 trace_extraction.py ../sgemm/sgemm_16w_50l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_50l
 python3 trace_extraction.py ../sgemm/sgemm_16w_100l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_100l
 python3 trace_extraction.py ../sgemm/sgemm_16w_200l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_200l
 python3 trace_extraction.py ../sgemm/sgemm_16w_400l/sgemm_double_buff_16w ./traces/sgemm_double_buff_16w_400l
+python3 trace_extraction.py ../sgemm/sgemm_32w_5l/sgemm_double_buff_32w ./traces/sgemm_double_buff_32w_5l
 python3 trace_extraction.py ../sgemm/sgemm_32w_10l/sgemm_double_buff_32w ./traces/sgemm_double_buff_32w_10l
 python3 trace_extraction.py ../sgemm/sgemm_32w_25l/sgemm_double_buff_32w ./traces/sgemm_double_buff_32w_25l
 python3 trace_extraction.py ../sgemm/sgemm_32w_50l/sgemm_double_buff_32w ./traces/sgemm_double_buff_32w_50l
@@ -54,6 +62,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_4w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_4w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_4w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_4w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_8w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_8w_200l /dev/null
@@ -61,6 +70,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_8w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_8w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_8w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_8w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_16w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_16w_200l /dev/null
@@ -68,6 +78,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_16w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_16w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_16w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_16w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_32w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_32w_200l /dev/null
@@ -75,6 +86,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_32w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_32w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_32w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_32w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_double_buff_4w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_double_buff_4w_200l /dev/null
@@ -82,6 +94,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_double_buff_4w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_double_buff_4w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_double_buff_4w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_double_buff_4w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_double_buff_8w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_double_buff_8w_200l /dev/null
@@ -89,6 +102,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_double_buff_8w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_double_buff_8w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_double_buff_8w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_double_buff_8w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_double_buff_16w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_double_buff_16w_200l /dev/null
@@ -96,6 +110,7 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_double_buff_16w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_double_buff_16w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_double_buff_16w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_double_buff_16w_5l /dev/null
 
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_400l_lrr/gpgpusim.config ./traces/sgemm_double_buff_32w_400l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_200l_lrr/gpgpusim.config ./traces/sgemm_double_buff_32w_200l /dev/null
@@ -103,3 +118,4 @@ python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_100l_lrr
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/sgemm_double_buff_32w_50l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/sgemm_double_buff_32w_25l /dev/null
 python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/sgemm_double_buff_32w_10l /dev/null
+python3 ../sass-decompiler/src/sgemm_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/sgemm_double_buff_32w_5l /dev/null
