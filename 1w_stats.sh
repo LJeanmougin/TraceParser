5793689f824a2ab5ffa2539fc9a46f2c0153a14e
LJeanmougin <louison.jeanmougin@irit.fr>
2025-10-29 15:23:41 +0100

    Updated the script for 1 warp evaluation.

--- 1w_stats.sh
+++ 1w_stats.sh
@@ -1,4 +1,5 @@
-python3.11 trace_extraction.py ../rodinia_1w_5l_fix_test ./traces/rodinia_1w_5l_fix_test
+python3.11 trace_extraction.py ../rodinia_1w_5l_alternative_test ./traces/rodinia_1w_5l_alternative_test
+# python3.11 trace_extraction.py ../rodinia_1w_5l_fix_test ./traces/rodinia_1w_5l_fix_test
 # python3.11 trace_extraction.py ../ecrts_1w_10l ./traces/ecrts_1w_10l
 # python3.11 trace_extraction.py ../ecrts_1w_25l ./traces/ecrts_1w_25l
 # python3.11 trace_extraction.py ../ecrts_1w_50l ./traces/ecrts_1w_50l
@@ -11,4 +12,5 @@ python3.11 trace_extraction.py ../rodinia_1w_5l_fix_test ./traces/rodinia_1w_5l_
 # python3.11 ../sass-decompiler/src/ptx_experiments.py ./configs/SM86_ORIN_50l_lrr/gpgpusim.config ./traces/ecrts_1w_50l /dev/null
 # python3.11 ../sass-decompiler/src/ptx_experiments.py ./configs/SM86_ORIN_25l_lrr/gpgpusim.config ./traces/ecrts_1w_25l /dev/null
 # python3.11 ../sass-decompiler/src/ptx_experiments.py ./configs/SM86_ORIN_10l_lrr/gpgpusim.config ./traces/ecrts_1w_10l /dev/null
-python3.11 ../sass-decompiler/src/ptx_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/rodinia_1w_5l_fix_test /dev/null
+# python3.11 ../sass-decompiler/src/ptx_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/rodinia_1w_5l_fix_test /dev/null
+python3.11 ../sass-decompiler/src/ptx_trace_experiments.py ./configs/SM86_ORIN_5l_lrr/gpgpusim.config ./traces/rodinia_1w_5l_alternative_test /dev/null
