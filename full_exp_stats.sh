abadaa0ebf378c1398e727d6cae5668e017cb98d
LJeanmougin <louison.jeanmougin@irit.fr>
2026-03-23 14:55:46 +0100

    Updated bounds

--- full_exp_stats.sh
+++ full_exp_stats.sh
@@ -4,7 +4,7 @@
 # python3.11 trace_extraction.py ../sim_lrr/rodinia_1b_50l ./traces_lrr/rodinia_1b_50l
 # python3.11 trace_extraction.py ../sim_lrr/rodinia_1b_100l ./traces_lrr/rodinia_1b_100l
 # python3.11 trace_extraction.py ../sim_lrr/rodinia_1b_200l ./traces_lrr/rodinia_1b_200l
-# python3.11 trace_extraction.py ../sim_gto/rodinia_1b_400l ./traces_gto/rodinia_1b_400l
+# python3.11 trace_extraction.py ../sim_lrr/rodinia_1b_400l ./traces_lrr/rodinia_1b_400l
 # python3.11 trace_extraction.py ../sim_gto/rodinia_1b_5l ./traces_gto/rodinia_1b_5l
 # python3.11 trace_extraction.py ../sim_gto/rodinia_1b_10l ./traces_gto/rodinia_1b_10l
 # python3.11 trace_extraction.py ../sim_gto/rodinia_1b_25l ./traces_gto/rodinia_1b_25l
